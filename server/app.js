c155ccf62ebec674ad66e4f5dfbdbbc9c5a9911f
luong.phuc <minhphuc86@gmail.com>
2020-05-19 09:40:17 +0000

    Add API USER

--- server/app.js
+++ server/app.js
@@ -16,14 +16,32 @@ app.set('view engine', 'ejs')
 // Set Public resource
 app.use(express.static('public'))
 
-
-// Routes
+// Routes Web
 const indexRouter = require('../routes/web/vue')
 app.use('/', indexRouter)
 
+// Routes Api
+app.use('/api/*', function (req, res, next) {
+    res.set('Content-Type', 'application/json')
+
+    return next()
+
+    // const bearerHeader = req.headers['authorization'];
+    // if (bearerHeader) {
+    //     console.log(`Token: ${bearerHeader}`)
+    //     // TODO: Check Token
+    //     return next()
+    // }
+
+    // return res.sendStatus(403)
+})
+
+const userApiRouter = require('../routes/api/userApi')
+app.use('/api/user', userApiRouter)
 
 
-const port = process.env.PORT || 8080
+// Configure Server
+const port = process.env.PORT_SERVER || 8080
 const host = process.env.HOST || '0.0.0.0'
 
 app.listen(port, host, () => console.log(`Server ${host} started on port ${port} .`) )
